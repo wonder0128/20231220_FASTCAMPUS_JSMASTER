8b4e57e34914b75f3b363b5ee6206e6b828e3127
roze <wonder_0128@naver.com>
2024-01-16 09:20:12 +0900

    20240116

--- 12/main.js
+++ 12/main.js
@@ -179,9 +179,54 @@
 
 //--------------------------------------------
 
-/** 클로저 */
+/** 클로저(Closure) */
+// 함수가 선언될 때의 유효범위(렉시컬 범위)를 기억하고 있다가
+// 함수가 외부에서 호출될 때 그 유효범위의 특정 변수를 참조할 수 있는 개념
 
+// function createCount() {
+//   let a = 0;
+//   return function() {
+//     return a+=1;
+//   }
+// }
 
+// const count = createCount(); //  클로저
+
+// console.log(count()); // 1
+// console.log(count()); // 2
+// console.log(count()); // 3
+
+// const count2 = createCount();
+// console.log(count2());
+// console.log(count2());
+
+// const h1El = document.querySelector('h1');
+// const h2El = document.querySelector('h2');
+
+// 별도의 상태 관리 필요
+// let h1IsRed = false;
+// let h2IsRed = false;
+
+// h1El.addEventListener('click', e => {
+//   h1IsRed = !h1IsRed;
+//   h1El.style.color = h1IsRed ? 'red' : 'black';
+// })
+
+// h2El.addEventListener('click', e => {
+//   h2IsRed = !h2IsRed;
+//   h2El.style.color = h2IsRed ? 'red' : 'black';
+// })
+
+// 하나의 함수로 정리
+// const createToggleHandler = () => {
+//   let isRed = false;
+//   return e => {
+//     isRed = !isRed;
+//     e.target.style.color = isRed ? 'red' : 'black';
+//   }
+// }
+// h1El.addEventListener('click', createToggleHandler());
+// h2El.addEventListener('click', createToggleHandler());
 
 //--------------------------------------------
 
