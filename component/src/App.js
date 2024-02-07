f13eb9388b6881c1c536e38dec882d39ef6a6f1d
roze <wonder_0128@naver.com>
2024-02-07 15:25:27 +0900

    20240206

---
+++ component/src/App.js
@@ -0,0 +1,26 @@
+import {Component} from './core/roze'
+
+export default class App extends Component {
+  constructor(){
+    super({
+      state: {
+        inputText : ''
+      }
+    })
+  }
+  render() {
+    this.el.classList.add('search');
+    this.el.innerHTML = /* html */ `
+    <input />
+    <button>Click!</button>
+    `
+    const inputEl = this.el.querySelector('input');
+    inputEl.addEventListener('input', () => {
+      this.state.inputText = inputEl.value;
+    })
+    const buttonEl = this.el.querySelector('button');
+    buttonEl.addEventListener('click', () => {
+      console.log(this.state.inputText);
+    })
+  }
+}
