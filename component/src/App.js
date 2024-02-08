ae69f86a2e3c3d939e4d9e434de2d9c266fba9b9
roze <110625211+wonder0128@users.noreply.github.com>
2024-02-08 14:25:08 +0900

    240207

--- component/src/App.js
+++ component/src/App.js
@@ -1,26 +1,34 @@
 import {Component} from './core/roze'
+import FruitItem from './components/FruitItem'
 
 export default class App extends Component {
   constructor(){
     super({
       state: {
-        inputText : ''
+        fruits: [
+          {name: 'Apple', price: 1000},
+          {name: 'Banana', price: 2000},
+          {name: 'Cherry', price: 3000}
+        ]
       }
     })
   }
   render() {
-    this.el.classList.add('search');
+    console.log(this.state.fruits)
     this.el.innerHTML = /* html */ `
-    <input />
-    <button>Click!</button>
+      <h1>Fruits</h1>
+      <ul></ul>
     `
-    const inputEl = this.el.querySelector('input');
-    inputEl.addEventListener('input', () => {
-      this.state.inputText = inputEl.value;
-    })
-    const buttonEl = this.el.querySelector('button');
-    buttonEl.addEventListener('click', () => {
-      console.log(this.state.inputText);
-    })
+    const ulEl = this.el.querySelector('ul');
+    
+    ulEl.append(...this.state.fruits
+      // .filter(fruit => fruit.price < 3000)
+      .map(fruit => new FruitItem({
+        props: {
+          name: fruit.name,
+          price: fruit.price
+        }
+      }).el)
+    )
   }
 }
